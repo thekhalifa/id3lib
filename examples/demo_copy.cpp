2322218e02c0c91b175b264090997582d9cea9ec
Ahmad Khalifa <ahmad@khalifa.ws>
2024-01-13 04:52:36 +0000

    id3cp: Remove expensive gengetopt and match id3info style

--- examples/demo_copy.cpp
+++ examples/demo_copy.cpp
@@ -1,54 +1,39 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
-
-
-#include "id3/tag.h"
-#include "id3/id3lib_streams.h"
-#include "demo_copy_options.h"
+#include <config.h>
+#include <id3/id3lib_streams.h>
+#include <id3/tag.h>
 #include <string.h>
+#include <vector>
 
+#define APP_NAME "id3cp"
 
 using std::cout;
 using std::endl;
 using std::cerr;
 
-static const char *VERSION_NUMBER = "$Revision: 1.11 $";
-
-void PrintUsage (const char *sName)
+/* Replaced gengetopt as with demo_info/id3info */
+void PrintUsage ()
 {
-    cout << "Usage: " << sName << " [OPTION]... SOURCE DEST" << endl;
-    cout << "Copy the tag(s) from SOURCE to DEST." << endl;
+    cout << "Copy one or all tags from SOURCE to DEST." << endl;
+    cout << endl;
+    cout << "Usage: " << APP_NAME << " [OPTIONS] SOURCE DEST" << endl;
+    cout << "       " << APP_NAME << " [-h | -V]" << endl;
+    cout << endl;
+    cout << "  -1  --v1tag     Render only the id3v1 tag (default=off)" << endl;
+    cout << "  -2  --v2tag     Render only the id3v2 tag (default=off)" << endl;
+    cout << endl;
+    cout << "  -h, --help      Print help and exit" << endl;
+    cout << "  -V, --version   Print version and exit" << endl;
     cout << endl;
 }
 
-void PrintVersion (const char *sName)
+void PrintVersion ()
 {
-    size_t nIndex;
-    cout << sName << " ";
-
-    for (nIndex = 0; nIndex < strlen (VERSION_NUMBER); nIndex++) {
-        if (VERSION_NUMBER[nIndex] == ' ') {
-            break;
-        }
-    }
-
-    nIndex++;
-
-    for (; nIndex < strlen (VERSION_NUMBER); nIndex++) {
-        if (VERSION_NUMBER[nIndex] == ' ') {
-            break;
-        }
-
-        cout << VERSION_NUMBER[nIndex];
-    }
-
+    cout << APP_NAME << " version: " << VERSION << endl;
+    cout << "Copy tags from one file to another." << endl;
+    cout << "Uses " << ID3LIB_FULL_NAME << endl;
     cout << endl;
-    cout << "Uses " << ID3LIB_FULL_NAME << endl << endl;
-
-    cout << "This program copies tags from one file to another" << endl;
 }
 
 void DisplayTags (ostream &os, luint nTags)
@@ -73,45 +58,40 @@ void DisplayTags (ostream &os, luint nTags)
 int main (int argc, char *const argv[])
 {
     int ulFlag = ID3TT_ID3;
-    ID3D_INIT_DOUT();
-    gengetopt_args_info args;
 
-    if (cmdline_parser (argc, argv, &args) != 0) {
+    std::vector<std::string> files;
+
+    for (size_t i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+
+        if (arg == "-h" || arg == "--help") {
+            PrintUsage();
+            exit (0);
+        } else if (arg == "-V" || arg == "--version") {
+            PrintVersion();
+            exit (0);
+        } else if (arg == "-1" || arg == "--v1tag") {
+            ulFlag = ID3TT_ID3V1;
+        } else if (arg == "-2" || arg == "--v2tag") {
+            ulFlag = ID3TT_ID3V2;
+        } else if (arg.at (0) == '-') {
+            cout << "Error, Invalid option: " << arg << endl << endl;
+            PrintUsage();
+            exit (1);
+        } else {
+            files.push_back (arg);
+        }
+    }
+
+    if (files.size() != 2) {
+        cout << "Error, Invalid number of SOURCE or DESTINATION: " << endl << endl;
+        PrintUsage();
         exit (1);
     }
 
-#if defined ID3_ENABLE_DEBUG
-
-    if (args.warning_flag) {
-        ID3D_INIT_WARNING();
-        ID3D_WARNING ("warnings turned on");
-    }
-
-    if (args.notice_flag) {
-        ID3D_INIT_NOTICE();
-        ID3D_NOTICE ("notices turned on");
-    }
-
-#endif
-
-    if (args.v1tag_flag) {
-        ulFlag = ID3TT_ID3V1;
-    }
-
-    if (args.v2tag_flag) {
-        ulFlag = ID3TT_ID3V2;
-    }
-
-
-    if (args.inputs_num != 2) {
-        cerr << "Usage: id3cp [OPTIONS] SOURCE DEST" << endl;
-        exit (1);
-    }
-
-    const char *source = args.inputs[0], *dest = args.inputs[1];
+    const char *source = files[0].c_str(), *dest = files[1].c_str();
 
     ID3_Tag myTag;
-
     cout << "Parsing " << source << ": ";
 
     myTag.Clear();
@@ -126,4 +106,3 @@ int main (int argc, char *const argv[])
 
     return 0;
 }
-
