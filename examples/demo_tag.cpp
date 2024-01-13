ba96afa807609f1ce45dbd41734e322d99a9970e
Ahmad Khalifa <ahmad@khalifa.ws>
2024-01-13 07:35:18 +0000

    id3tag: Remove expensive gengetopt and match id3info style

--- examples/demo_tag.cpp
+++ examples/demo_tag.cpp
@@ -1,37 +1,56 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
+#include <config.h>
 
-#include "id3/id3lib_streams.h"
+#include <id3/id3lib_streams.h>
+#include <id3/misc_support.h>
+#include <id3/tag.h>
+#include <map>
 #include <stdlib.h>
 #include <string.h>
+#include <vector>
 
-#include <id3/tag.h>
-#include <id3/misc_support.h>
-
-#include "demo_tag_options.h"
+#define APP_NAME "id3tag"
 
 using std::cout;
 using std::endl;
+using std::string;
 
-static const char *VERSION_NUMBER = "$Revision: 1.15 $";
-
-void PrintUsage (const char *sName)
+/* Replaced gengetopt as with demo_info/id3info */
+void PrintUsage ()
 {
-    cout << "Will render both types of tag by default.  Only the last" << endl
-         << "tag type indicated in the option list will be used.  Non-" << endl
-         << "rendered will remain unchanged in the original file.  Will" << endl
-         << "also parse and convert Lyrics3 v2.0 frames, but will not" << endl
-         << "render them." << endl;
+    cout << "Will render both types of tag by default.  Only the last" << endl;
+    cout << "tag type indicated in the option list will be used.  Non-" << endl;
+    cout << "rendered will remain unchanged in the original file.  Will" << endl;
+    cout << "also parse and convert Lyrics3 v2.0 frames, but will not" << endl;
+    cout << "render them." << endl;
+    cout << endl;
+    cout << "Usage: " << APP_NAME << " [OPTIONS]... <FILE>..." << endl;
+    cout << "       " << APP_NAME << " [-h | -V]" << endl;
+    cout << endl;
+    cout << "  -aSTRING   --artist=STRING   Set the artist information" << endl;
+    cout << "  -ASTRING   --album=STRING    Set the album title information" << endl;
+    cout << "  -sSTRING   --song=STRING     Set the title information" << endl;
+    cout << "  -cSTRING   --comment=STRING  Set the comment information" << endl;
+    cout << "  -CSTRING   --desc=STRING     Set the comment description" << endl;
+    cout << "  -ySTRING   --year=STRING     Set the year" << endl;
+    cout << "  -tSTRING   --track=STRING    Set the track number" << endl;
+    cout << "  -TSTRING   --total=STRING    Set the total number of tracks" << endl;
+    cout << "  -gSHORT    --genre=SHORT     Set the genre" << endl;
+    cout << "  -1  --v1tag     Render only the id3v1 tag (default=off)" << endl;
+    cout << "  -2  --v2tag     Render only the id3v2 tag (default=off)" << endl;
+    cout << endl;
+    cout << "  -h, --help      Print help and exit" << endl;
+    cout << "  -V, --version   Print version and exit" << endl;
+    cout << endl;
 }
 
-void PrintVersion (const char *sName)
+void PrintVersion ()
 {
-    cout << "Uses " << ID3LIB_FULL_NAME << endl << endl;
-
-    cout << "This program tags mp3 files with ID3v1/1.1 and/or id3v2 tags" << endl;
+    cout << APP_NAME << " version: " << VERSION << endl;
+    cout << "Tags mp3 files with ID3v1/1.1 and/or id3v2 tags." << endl;
+    cout << "Uses " << ID3LIB_FULL_NAME << endl;
+    cout << endl;
 }
 
 void DisplayTags (ostream &os, luint nTags)
@@ -55,138 +74,170 @@ void DisplayTags (ostream &os, luint nTags)
 
 int main (int argc, char *const argv[])
 {
-    int ulFlag = ID3TT_ID3;
-    ID3D_INIT_DOUT();
-    gengetopt_args_info args;
+    // int ulFlag = ID3TT_ID3;
 
-    if (cmdline_parser (argc, argv, &args) != 0) {
+    if (argc < 2) {
+        PrintUsage();
         exit (1);
     }
 
-#if defined ID3_ENABLE_DEBUG
+    int ulFlag = ID3TT_ID3;
 
-    if (args.warning_flag) {
-        ID3D_INIT_WARNING();
-        ID3D_WARNING ("warnings turned on");
-    }
-
-    if (args.notice_flag) {
-        ID3D_INIT_NOTICE();
-        ID3D_NOTICE ("notices turned on");
-    }
-
-#endif
-
-    if (args.v1tag_flag) {
-        ulFlag = ID3TT_ID3V1;
-    }
-
-    if (args.v2tag_flag) {
-        ulFlag = ID3TT_ID3V2;
-    }
-
-    const char
-    *sArtist  = "",
-     *sAlbum   = "",
-      *sTitle   = "",
-       *sComment = "",
-        *sYear    = "",
-         *sDesc    = "";
-    unsigned short
-    nYear     = 0,
-    nTrack    = 0,
-    nTotal    = 0,
-    nGenre    = 0;
+    std::map<string, string> argmap;
+    argmap["a"] = "ARTIST";
+    argmap["artist"] = "ARTIST";
+    argmap["A"] = "ALBUM";
+    argmap["album"] = "ALBUM";
+    argmap["s"] = "SONG";
+    argmap["song"] = "SONG";
+    argmap["c"] = "COMMENT";
+    argmap["comment"] = "COMMENT";
+    argmap["C"] = "DESCRIPTION";
+    argmap["desc"] = "DESCRIPTION";
+    argmap["y"] = "YEAR";
+    argmap["year"] = "YEAR";
+    argmap["t"] = "TRACK";
+    argmap["track"] = "TRACK";
+    argmap["T"] = "TOTAL";
+    argmap["total"] = "TOTAL";
+    argmap["g"] = "GENRE";
+    argmap["genre"] = "GENRE";
 
 
-    if (args.artist_given) {
-        sArtist = args.artist_arg;
-        cout << "+++ Artist  = " << sArtist << endl;
-    }
+    std::vector<string> files;
+    std::map<string, string> tagmap;
+    string key, value, tagkey;
+    string::size_type sep;
 
-    if (args.album_given) {
-        sAlbum = args.album_arg;
-        cout << "+++ Album   = " << sAlbum << endl;
-    }
+    for (size_t i = 1; i < argc; i++) {
+        std::string arg = argv[i];
 
-    if (args.song_given) {
-        sTitle = args.song_arg;
-        cout << "+++ Song    = " << sTitle << endl;
-    }
+        if (arg == "-h" || arg == "--help") {
+            PrintUsage();
+            exit (0);
+        } else if (arg == "-V" || arg == "--version") {
+            PrintVersion();
+            exit (0);
+        } else if (arg == "-1" || arg == "--v1tag") {
+            ulFlag = ID3TT_ID3V1;
+        } else if (arg == "-2" || arg == "--v2tag") {
+            ulFlag = ID3TT_ID3V2;
+            // process long options first --artist=X
+        } else if (arg[0] == '-' && arg[1] == '-') {
+            sep = arg.find ("=");
 
-    if (args.year_given) {
-        sYear = args.year_arg;
-        nYear = ::strtol (sYear, NULL, 10);
-        cout << "+++ Year    = " << nYear << endl;
-    }
+            if (sep < 3)
+                continue;
 
-    if (args.comment_given) {
-        sComment = args.comment_arg;
-        cout << "+++ Comment = " << sComment << endl;
+            key = arg.substr (2, sep - 2);
+            value = arg.substr (sep + 1);
+            tagkey = argmap[key];
 
-        if (args.desc_given) {
-            sDesc = args.desc_arg;
-            cout << "+++ Comment Description" << endl;
-            cout << "            = " << sDesc << endl;
+            if (tagkey == key) {
+                cout << "Invalid key: " << key << endl;
+                continue;
+            }
+
+            tagmap[tagkey] = value;
+            // process short options -aARTIST_NAME
+        } else if (arg[0] == '-') {
+            key = arg[1];
+            value = arg.substr (2);
+            tagkey = argmap[key];
+
+            if (tagkey == key) {
+                cout << "Invalid key: " << key << endl;
+                continue;
+            }
+
+            tagmap[tagkey] = value;
         } else {
-            sDesc = "";
+            files.push_back (arg);
         }
     }
 
-    if (args.genre_given && args.genre_arg > 0 && args.genre_arg < 0xFF) {
-        nGenre = args.genre_arg;
-        cout << "+++ Genre   = " << args.genre_arg << endl;
-    }
+    std::map<string, string>::iterator mapit;
+    std::vector<std::string>::iterator i;
 
-    if (args.track_given) {
-        nTrack = ::strtol (args.track_arg, NULL, 10);
-        cout << "+++ Track   = " << nTrack << endl;
-    }
-
-    if (args.total_given) {
-        nTotal = ::strtol (args.total_arg, NULL, 10);
-        cout << "+++ Total   = " << nTotal << endl;
-    }
-
-    const char *filename = NULL;
-
-    for (size_t i = 0; i < args.inputs_num; ++i) {
-        filename = args.inputs[i];
+    for (i = files.begin(); i != files.end(); i++) {
+        std::string filename = *i;
         ID3_Tag myTag;
 
         cout << "Tagging " << filename << ": ";
 
-        myTag.Link (filename);
+        myTag.Link (filename.c_str());
 
         cout << "attempting ";
         DisplayTags (cout, ulFlag);
 
-        if (args.artist_given) {
-            ID3_AddArtist (&myTag, sArtist, true);
+        mapit = tagmap.find ("ARTIST");
+
+        if (mapit != tagmap.end()) {
+            ID3_AddArtist (&myTag, (*mapit).second.c_str(), true);
+            cout << "+++ Artist  = " << (*mapit).second << endl;
         }
 
-        if (args.album_given) {
-            ID3_AddAlbum (&myTag, sAlbum, true);
+        mapit = tagmap.find ("ALBUM");
+
+        if (mapit != tagmap.end()) {
+            ID3_AddAlbum (&myTag, (*mapit).second.c_str(), true);
+            cout << "+++ Album  = " << (*mapit).second << endl;
         }
 
-        if (args.song_given) {
-            ID3_AddTitle (&myTag, sTitle, true);
+        mapit = tagmap.find ("SONG");
+
+        if (mapit != tagmap.end()) {
+            ID3_AddTitle (&myTag, (*mapit).second.c_str(), true);
+            cout << "+++ Song  = " << (*mapit).second << endl;
         }
 
-        if (args.year_given) {
-            ID3_AddYear (&myTag, sYear, true);
+        mapit = tagmap.find ("YEAR");
+
+        if (mapit != tagmap.end()) {
+            ID3_AddYear (&myTag, (*mapit).second.c_str(), true);
+            cout << "+++ Year  = " << (*mapit).second << endl;
         }
 
-        if (args.comment_given) {
-            ID3_AddComment (&myTag, sComment, sDesc, true);
+        mapit = tagmap.find ("COMMENT");
+
+        if (mapit != tagmap.end()) {
+            value = (*mapit).second;
+            string desc;
+            mapit = tagmap.find ("DESCRIPTION");
+
+            if (mapit != tagmap.end())
+                desc = (*mapit).second;
+
+            ID3_AddComment (&myTag, value.c_str(), desc.c_str(), true);
+            cout << "+++ Comment = " << value << endl;
+
+            if (desc.length()) {
+                cout << "+++ Comment Description" << endl;
+                cout << "            = " << desc << endl;
+            }
         }
 
-        if (args.genre_given) {
-            ID3_AddGenre (&myTag, nGenre, true);
+        mapit = tagmap.find ("GENRE");
+
+        if (mapit != tagmap.end()) {
+            unsigned short genre = ::strtol ( (*mapit).second.c_str(), NULL, 10);
+            ID3_AddGenre (&myTag, genre, true);
+            cout << "+++ Genre  = " << genre << endl;
         }
 
-        if (args.track_given) {
-            ID3_AddTrack (&myTag, nTrack, nTotal, true);
+        mapit = tagmap.find ("TRACK");
+
+        if (mapit != tagmap.end()) {
+            unsigned short track = ::strtol ( (*mapit).second.c_str(), NULL, 10);
+            string totalstr;
+            mapit = tagmap.find ("TOTAL");
+
+            if (mapit != tagmap.end())
+                totalstr = (*mapit).second;
+
+            unsigned short total = ::strtol (totalstr.c_str(), NULL, 10);
+            ID3_AddTrack (&myTag, track, total, true);
+            cout << "+++ Track = " << track << endl;
         }
 
         luint nTags = myTag.Update (ulFlag);
@@ -199,4 +250,3 @@ int main (int argc, char *const argv[])
 
     return 0;
 }
-
